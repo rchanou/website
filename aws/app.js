5d187d341c56f80eabfa1e3beb5968e4d0cb8109
Ronald Chanou <rchanou@gmail.com>
2017-03-12 13:41:15 -0700

    feat: prelim captcha handling code

--- aws/app.js
+++ aws/app.js
@@ -1,7 +1,12 @@
 const path = require("path");
 const lambdaLocal = require("lambda-local");
 
-const event = {};
+const event = {
+  body: JSON.stringify({
+    test: "trst",
+    "g-recaptcha-response":"03AI-r6f5RKIkrAIuC0Xx9kG_-Xb4olG3-L98ArOTNr_fmOgzSwpg045dmYsn-oC5exGTPmBYFUu6cL27Y4sbmIxC-piEmXEdOPt9Ki6eCL89nmm5DKYmfgSR_z-S2sShU2nB-J-rIhr0OTKdSxCRG_KIX43mnrGJ3IzJRwafW8XWja-hlumv8dyMmX8lvtHElB-Me1xVPNpcYGE0pcXQ3dSpgUKwb0HckktFax-bf3aQ1049gnHVDprVdWyKxbbbHVQbJW-JwZKiDHPsLlt3g5blR3QJfwdta08OZbOTaAm-7_l7xr_0pDRqU7R3M4lPluM-m2Hs6K0G1M3glpGKL2YAoVlvZvNzyWpHxDMoaIx8fokoG2R-SiWc"
+  })
+};
 
 lambdaLocal.execute({
   event,
--- aws/check-captcha/app.js
+++ aws/check-captcha/app.js
@@ -1,3 +1,43 @@
+const request = require("request");
+
+const headers = {
+  "Content-Type": "application/json",
+  "Access-Control-Allow-Headers": "Accept,Content-Type,Access-Control-Allow-Origin",
+  "Access-Control-Allow-Origin": "*"
+};
+
 exports.handler = (event, context, callback) => {
-  callback(null, "Hello from Lambda");
+  const handleResponse = (err, res, body) => {
+    const bodyData = JSON.parse(body);
+    mmconsole.log('body', bodyData);
+    if (err || !bodyData.success) {
+      callback(null, {
+        statusCode: "401",
+        body,
+        headers
+      });
+      return;
+    }
+
+    callback(null, {
+      statusCode: "200",
+      body,//: event.body,
+      headers
+    });
+  };
+
+  const bodyData = JSON.parse(event.body);
+
+  const captchaFormData = {
+    secret: process.env.recaptchaSecret,
+    response: bodyData["g-recaptcha-response"]
+  };
+
+  //console.log("cap", captchaFormData);
+
+  request.post(
+    " https://www.google.com/recaptcha/api/siteverify",
+    { Accept: 'application/json', form: captchaFormData },
+    handleResponse
+  );
 };
