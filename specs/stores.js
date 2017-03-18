3019e340245c5e89dbc5be0185d530b276f0127a
Ronald Chanou <rchanou@gmail.com>
2017-03-18 10:18:45 -0700

    feat: js.spec test, babel-node

---
+++ specs/stores.js
@@ -0,0 +1,52 @@
+import * as stores from "../src/stores";
+
+import { spec, valid, explain, conform } from "js.spec";
+
+const point = spec.map({
+  x: spec.int,
+  y: spec.int
+});
+
+const line = spec.map({
+  p1: point,
+  p2: point
+});
+
+const point_or_line = spec.or({
+  point,
+  line
+});
+
+const p = {
+  x: 0, y: 'nope'
+};
+valid(point_or_line, p);
+// => false
+
+// what is wrong?
+explain(point_or_line, p);
+// Or(point, line) → point → Map → Keys(x, y): hasKey failed for undefined at [y].
+// Or(point, line) → line → Map → Keys(p1, p2): hasKey failed for undefined at [p1].
+// Or(point, line) → line → Map → Keys(p1, p2): hasKey failed for undefined at [p2].
+
+// let's try again
+const p2 = {
+  x: 0,
+  y: 0
+};
+
+valid(point_or_line, p2);
+// => true
+
+// but which spec did it match?
+conform(point_or_line, p2);
+// => [ 'point', { x: 0, y: 0 } ]
+
+// regexes work also (only cat/alt currently though)
+const line2 = spec.cat("source", point, "target", point);
+conform(line2, [p2, p2]);
+// => {source: {x: 0, y: 0}, target: {x: 0, y: 0}}
+explain(line2, [p2, p]);
+// cat · → target → Map → Keys(x, y): hasKey failed for undefined at [1, y]
+explain(line2, [p2]);
+// cat · → target → Map: isObject failed for undefined at [1].
